cf11eb406ad429f68f25891d1ee55a865caab746
siddy17 <siddhantparashar17@gmail.com>
2024-05-23 16:42:25 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,15 @@
+let x= 49
+let y=48
+console.log(x+y)
+
+let k
+const age= 244
+console.log(age)
+/*
+let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
+seas.push('Red Sea');*/
+console.log(44+44);
+console.log(seas); 
+//this method is use to clear the console
+console.clear()
+
